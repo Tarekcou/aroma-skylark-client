15cd3391d86f46a04c889603823ad7546db042aa
tarek <sitarek77@gmail.com>
2025-07-13 03:52:57 +0600

    done

--- src/axios/AxiosPublic.js
+++ src/axios/AxiosPublic.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:5005/api",
+  // baseURL: "http://localhost:5005/api",
+  baseURL: "https://aroma-skylark-server.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
